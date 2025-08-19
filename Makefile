7b73ff4fb53e3a40e9ba6cb0fae89bb9f9de3a0a
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-19 12:20:48 +0800

    uploading first version

---
+++ Makefile
@@ -0,0 +1,43 @@
+NAME		= fdf
+LIBFT		= libft.a
+
+CC			= cc
+CFLAGS		= -Wall -Wextra -Werror -g3
+IFLAGS		= -Ilibft/include -Iinclude
+MLX_FLAGS	= -Lmlx_linux -lmlx_Linux -L/usr/lib -Imlx_linux -lXext -lX11 -lm -lz
+
+SRC		= \
+		  main.c
+
+LIBFT_DIR	= libft/
+SRC_DIR		= src/
+
+vpath %.c $(SRC_DIR)
+
+GREEN	= \e[32m
+CYAN	= \e[36m
+WHITE	= \e[0m
+
+.PHONY: all fclean clean re
+
+all: $(NAME)
+
+$(NAME): $(SRC) $(LIBFT)
+	@$(CC) $(CFLAGS) $^ $(IFLAGS) $(MLX_FLAGS) -o $@
+	@echo "Compiling $(GREEN)$(NAME)$(WHITE)..."
+
+$(LIBFT):
+	@echo "Making $(GREEN)$(LIBFT)$(WHITE)..."
+	@make -C $(LIBFT_DIR)
+	@cp $(addprefix $(LIBFT_DIR), $(LIBFT)) $(LIBFT)
+
+fclean: clean
+	@rm -f $(NAME)
+	@echo "Removing program files..."
+
+clean:
+	@make -C $(LIBFT_DIR) clean
+	@rm -f $(LIBFT) $(LIBFT_DIR)/$(LIBFT)
+	@echo "Removing $(GREEN)$(LIBFT)$(WHITE)..."
+
+re: fclean all
---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 11:56:09 by kchiang           #+#    #+#             */
+/*   Updated: 2025/08/19 12:15:37 by kchiang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <mlx.h>
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_window;
+
+	mlx = mlx_init();
+	mlx_window = mlx_new_window(mlx, 1920, 1080, "fdf");
+	mlx_loop(mlx);
+	(void)mlx_window;
+}
