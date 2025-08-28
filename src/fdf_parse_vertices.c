70ab0415828404df5e2d0a0f669591f28af5e749
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-08-28 19:36:45 +0800

    finished fn_get_color in parse_vertices

--- src/fdf_parse_vertices.c
+++ src/fdf_parse_vertices.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/28 13:41:20 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/28 17:21:32 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/28 19:35:25 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,7 @@ static void	fn_extract_coord(char *line, t_map *map, int i, int z)
 	{
 		map->vertices[i + x].y = ft_atoi(arr[x]);
 		map->vertices[i + x].color = fn_get_color(arr[x]);
+		x++;
 	}
 }
 
@@ -66,7 +67,11 @@ static int	fn_get_color(char *s)
 		s++;
 		if (ft_strncmp(s, "0x", 2) || ft_strncmp(s, "0X", 2))
 			return (PIXEL_BLUE);
-		/* ft_atoi_base */
+		while (*s == '+' || *s == '-')
+			s++;
+		if (!ft_strncmp(s, "0x", 2) || !ft_strncmp(s, "0X", 2))
+			s += 2;
+		color = ft_atoi_base(s);
 	}
 	return (color);
 }
