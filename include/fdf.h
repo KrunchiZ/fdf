00e94d936480e19fe01820a014d5d0498038ca2b
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-08-31 01:13:00 +0800

    change res to 1080x1080

--- include/fdf.h
+++ include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 12:51:26 by kchiang           #+#    #+#             */
-/*   Updated: 2025/08/31 01:08:17 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/08/31 01:12:38 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 
 # define FN_SUCCESS		0
 # define FN_FAILURE		1
-# define FRAME_WIDTH	1440
-# define FRAME_HEIGHT	1440
+# define FRAME_WIDTH	1080
+# define FRAME_HEIGHT	1080
 
 # define PIXEL_WHITE	0xFFFFFF
 # define PIXEL_GREY		0x757575
