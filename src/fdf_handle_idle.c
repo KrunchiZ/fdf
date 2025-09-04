8f73b134a044f75eac46ffc9c5aa80f9b3eb1ad6
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-09-04 14:54:51 +0800

    fix spelling

--- src/fdf_handle_idle.c
+++ src/fdf_handle_idle.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 14:47:42 by kchiang           #+#    #+#             */
-/*   Updated: 2025/09/04 14:53:20 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/09/04 14:54:37 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	handle_idle(t_data *data)
 {
-	handle_keypress_transfrom();
+	handle_keypress_transform();
 	if (data->window)
 	{
 		transform_map(&data->map, &data->mod);
