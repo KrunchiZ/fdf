7a14627a4ed1fddf8f82c559fa2a0928265b34c4
kz_laptop_T14G5 <kaizhi3006@gmail.com>
2025-09-05 23:45:10 +0800

    finished rotate_matrix

--- src/fdf_rotate_matrix.c
+++ src/fdf_rotate_matrix.c
@@ -6,43 +6,43 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/05 00:32:17 by kchiang           #+#    #+#             */
-/*   Updated: 2025/09/05 18:23:48 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/09/05 23:44:10 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void	update_matrix(t_mtrx *matrix, t_vect *viewangle, t_vect *rotate);
+static void	update_sin_cos(t_mtrx *mx, t_vect *viewangle, t_vect *rotate);
 
 void	calc_rotate_matrix(t_mod *mod)
 {
-	update_matrix
+	update_sin_cos
 		(&mod->rotate_matrix, &mod->viewangle[mod->viewmode], &mod->rotate);
-	multiply_matrix
-		(mod->rotate_matrix.zy, mod->rotate_matrix.z, mod->rotate_matrix.y);
-	multiply_matrix
-		(mod->rotate_matrix.zyx, mod->rotate_matrix.zy, mod->rotate_matrix.x);
+	calc_zyx_matrix(&mod->rotate_matrix, mod->rotate_matrix.zyx);
 	return ;
 }
 
-static void	update_matrix(t_mtrx *matrix, t_vect *viewangle, t_vect *rotate)
+static void	update_sincos(t_mtrx *mx, t_vect *viewangle, t_vect *rotate)
 {
-	matrix->z[0][0] = cos(viewangle->z + rotate->z);
-	matrix->z[1][0] = sin(viewangle->z + rotate->z);
-	matrix->z[0][1] = -(matrix->z[1][0]);
-	matrix->z[1][1] = matrix->z[0][0];
-	matrix->y[0][0] = cos(viewangle->y + rotate->y);
-	matrix->y[0][2] = sin(viewangle->y + rotate->y);
-	matrix->y[2][0] = -(matrix->y[0][2]);
-	matrix->y[2][2] = matrix->y[0][0];
-	matrix->x[1][1] = cos(viewangle->x + rotate->x);
-	matrix->x[2][1] = sin(viewangle->x + rotate->x);
-	matrix->x[1][2] = -(matrix->x[2][1]);
-	matrix->x[2][2] = matrix->x[1][1];
+	mx->zcos = cos(viewangle->z + rotate->z);
+	mx->zsin = sin(viewangle->z + rotate->z);
+	mx->ycos = cos(viewangle->y + rotate->y);
+	mx->ysin = sin(viewangle->y + rotate->y);
+	mx->xcos = cos(viewangle->x + rotate->x);
+	mx->xsin = sin(viewangle->x + rotate->x);
 	return ;
 }
 
-static void	multiply_matrix(float **mc, float **ma, float **mb)
+static void	calc_zyx_matrix(t_mtrx *mx, float **zyx)
 {
-	mc = ma * mb;
+	mx[2][0] = -(mx->ysin);
+	mx[2][1] = mx->ycos * mx->xsin;
+	mx[2][2] = mx->ycos * mx->xcos;
+	mx[0][0] = mx->zcos * mx->ycos;
+	mx[1][0] = mx->zsin * mx->ycos;
+	mx[0][1] = (mx->zcos * mx->ysin * mx->xsin) - (mx->zsin * mx->xcos);
+	mx[0][2] = (mx->zcos * mx->ysin * mx->xcos) + (mx->zsin * mx->xsin);
+	mx[1][1] = (mx->zsin * mx->ysin * mx->xsin) + (mx->zcos * mx->xcos);
+	mx[1][2] = (mx->zsin * mx->ysin * mx->xcos) - (mx->zcos * mx->xsin);
+	return ;
 }
