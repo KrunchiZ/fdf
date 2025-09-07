fb127d03313b35e286c31e1d4dc8227640944ee2
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-09-07 15:23:02 +0800

    updated draw_map function in render_img.c to render edges instead of render_pt

--- src/fdf_render_img.c
+++ src/fdf_render_img.c
@@ -6,15 +6,14 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 12:35:59 by kchiang           #+#    #+#             */
-/*   Updated: 2025/09/06 23:05:59 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/09/07 15:20:42 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+static void	draw_bg(t_data *data);
 static void	draw_map(t_img *img, t_map *map);
-static void	draw_rows(t_img *img, t_map *map, int i);
-static void	draw_columns(t_img *img, t_map *map, int i);
 
 void	img_px_put(t_img *img, int x, int y, int color)
 {
@@ -36,6 +35,14 @@ void	img_px_put(t_img *img, int x, int y, int color)
 
 int	render_img(t_data *data)
 {
+	draw_bg(data);
+	draw_map(&data->img, &data->map);
+	mlx_put_image_to_window(data->mlx, data->window, data->img.img_ptr, 0, 0);
+	return (SUCCESS);
+}
+
+static void	draw_bg(t_data *data)
+{
 	int	x;
 	int	y;
 	int	bg_color[4];
@@ -52,9 +59,7 @@ int	render_img(t_data *data)
 			img_px_put(&data->img, x++, y, bg_color[data->mod.bg]);
 		y++;
 	}
-	draw_map(&data->img, &data->map);
-	mlx_put_image_to_window(data->mlx, data->window, data->img.img_ptr, 0, 0);
-	return (SUCCESS);
+	return ;
 }
 
 static void	draw_map(t_img *img, t_map *map)
@@ -62,44 +67,10 @@ static void	draw_map(t_img *img, t_map *map)
 	int	i;
 
 	i = 0;
-	while (i < map->vertex_count)
+	while (i < map->edge_count)
 	{
-		draw_rows(img, map, i);
-		i += map->width;
-	}
-	i = 0;
-	while (i < map->width)
-		draw_columns(img, map, i++);
-	return ;
-}
-
-static void	draw_rows(t_img *img, t_map *map, int i)
-{
-	int	j;
-	int	count;
-
-	j = 0;
-	count = map->width - 1;
-	while (j < count)
-	{
-		draw_line(img, map->render_pt[i + j], map->render_pt[i + j + 1]);
-		j++;
-	}
-	return ;
-}
-
-static void	draw_columns(t_img *img, t_map *map, int i)
-{
-	int	j;
-	int	count;
-
-	j = 0;
-	count = map->vertex_count - map->width;
-	while (j < count)
-	{
-		draw_line(img, map->render_pt[i + j],
-				map->render_pt[i + j + map->width]);
-		j += map->width;
+		draw_line(img, map->edges[i].start, map->edges[i].end);
+		i++;
 	}
 	return ;
 }
