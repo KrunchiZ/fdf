a778cce49021714456cf34986c1dbcfebea4178e
Kai Zhi Chiang <kchiang@u91z03s03.42kl.edu.my>
2025-09-07 17:13:11 +0800

    added extra tab for control messages

--- src/fdf_main.c
+++ src/fdf_main.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 11:56:09 by kchiang           #+#    #+#             */
-/*   Updated: 2025/09/06 02:25:08 by kchiang          ###   ########.fr       */
+/*   Updated: 2025/09/07 17:12:51 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -83,12 +83,12 @@ static void	setup_mlx_loop(t_data *data)
 
 static void	print_first_message(void)
 {
-	ft_putendl_fd("\n+:+#+ FDF Controls +#+:+\n\n[esc]\t= Exit\n"
-		"[b]\t= Change background color\n[scroll]= Uniform scaling\n"
-		"[left]\t= ++Rotate-Y\n[right]\t= --Rotate-Y\n"
-		"[up]\t= ++Rotate-X\n[down]\t= --Rotate-X\n[,]\t= ++Rotate-Z\n"
-		"[.]\t= --Rotate-Z\n[mouse1]= (Hold & Drag) 2DCamera-Pan\n"
-		"[u]\t= Reset transform\n", STDOUT_FILENO);
+	ft_putendl_fd("\n+:+#+ FDF Controls +#+:+\n\n[esc]\t\t= Exit\n"
+		"[b]\t\t= Change background color\n[scroll]= Uniform scaling\n"
+		"[left]\t\t= ++Rotate-Y\n[right]\t\t= --Rotate-Y\n"
+		"[up]\t\t= ++Rotate-X\n[down]\t\t= --Rotate-X\n[,]\t\t= ++Rotate-Z\n"
+		"[.]\t\t= --Rotate-Z\n[mouse1]= (Hold & Drag) 2DCamera-Pan\n"
+		"[u]\t\t= Reset transform\n", STDOUT_FILENO);
 	ft_putendl_fd("Viewmode: Isometric", STDOUT_FILENO);
 	return ;
 }
