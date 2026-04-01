8058cb7a480085f266da52c0a4cde2aa9299a362
Kai Zhi Chiang <kchiang@u80z02s03.42kl.edu.my>
2026-04-01 11:21:17 +0800

    removed the small text on window, added colors to 42map

--- src/fdf_handle_idle.c
+++ src/fdf_handle_idle.c
@@ -6,7 +6,7 @@
 /*   By: kchiang <kchiang@student.42kl.edu.my>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/04 14:47:42 by kchiang           #+#    #+#             */
-/*   Updated: 2026/04/01 11:03:13 by kchiang          ###   ########.fr       */
+/*   Updated: 2026/04/01 11:17:05 by kchiang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 static void	handle_keypress_xtransform(t_mod *mod, int keyhold);
 static void	handle_keypress_ytransform(t_mod *mod, int keyhold);
 static void	handle_keypress_ztransform(t_mod *mod, int keyhold);
-static void	print_message(t_data *data);
 
 int	handle_idle(t_data *data)
 {
@@ -33,36 +32,8 @@ int	handle_idle(t_data *data)
 			data->mod.idle = true;
 		}
 		render_img(data);
-		print_message(data);
 	}
 	return (SUCCESS);
-}
-
-static void	print_message(t_data *data)
-{
-	mlx_string_put(data->mlx, data->window, 10, 30, PIXEL_BLUE,
-		" +:+#+ FDF Controls +#+:+ ");
-	mlx_string_put(data->mlx, data->window, 10, 60, PIXEL_BLUE,
-		" [esc]    = Exit           [u] = Reset transform");
-	mlx_string_put(data->mlx, data->window, 10, 80, PIXEL_BLUE,
-		" [up]     = --Rotate-X     [down] = ++Rotate-X");
-	mlx_string_put(data->mlx, data->window, 10, 100, PIXEL_BLUE,
-		" [left]   = --Rotate-Y     [right] = ++Rotate-Y");
-	mlx_string_put(data->mlx, data->window, 10, 120, PIXEL_BLUE,
-		" [,]      = ++Rotate-Z     [.] = --Rotate-Z");
-	mlx_string_put(data->mlx, data->window, 10, 140, PIXEL_BLUE,
-		" [mouse1]       = 2D Camera Pan");
-	mlx_string_put(data->mlx, data->window, 10, 160, PIXEL_BLUE,
-		" [r] + [mouse1] = Rotate-Y");
-	mlx_string_put(data->mlx, data->window, 10, 180, PIXEL_BLUE,
-		" [scroll]       = Uniform scaling");
-	mlx_string_put(data->mlx, data->window, 10, 200, PIXEL_BLUE,
-		" [x]/[y]/[z] + [scroll] = single axis scaling");
-	mlx_string_put(data->mlx, data->window, 10, 220, PIXEL_BLUE,
-		" [b]      = Change background color");
-	mlx_string_put(data->mlx, data->window, 10, 240, PIXEL_BLUE,
-		" [a]      = Auto Rotate");
-	return ;
 }
 
 static void	handle_keypress_xtransform(t_mod *mod, int keyhold)
